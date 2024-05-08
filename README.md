8dc4020993bca412b0a39bd7a118f4031d8e6bc8
brfc <brfc.email@gmail.com>
2024-05-08 12:23:56 +0100

    libecho: first commit  - first draft of header only library, including basic features  such as logging, call trace under condition failure, and basic  performance measurement for code blocks.  - mostly implemented under macros.

---
+++ README.md
@@ -0,0 +1,21 @@
+## libecho
+    * libecho is a header-only library to perform plug-and-play debug.
+    * Currently under development
+    * Personal usage ONLY
+
+#### Principles
+    * Results reported as text-based log events
+    * Plug and play using header-only libraries
+    * No extra-dependencies
+
+#### Aim features for 1st release
+    * Support log for variables and expressions
+    * Verbose mode
+    * Expectation conditions analysis
+    * Trance calls
+    * Thread profiling
+    * CPU profiling
+    * Memory profiling
+    * I/O profiling
+    * Distributed as conan package
+    * Doxygen & gitpage
---
+++ test/test.cpp
@@ -0,0 +1,57 @@
+#include "../include/libecho.hpp"
+#include <unistd.h>
+
+
+int fun4(void)
+{
+    int i = 10;
+
+    log("fun4 logging");
+    expect(i == 10);
+    expect(i == 20);
+
+    return 0;
+}
+
+int fun3(void)
+{
+    return fun4();
+}
+
+int fun2(void)
+{
+    return fun3();
+}
+
+int fun(void)
+{
+    return fun2();
+}
+
+int main()
+{
+    int i;
+    int b;
+
+    perf({ 
+        i = 10;
+        int l = 10;
+        int* a = new int(29);
+        usleep(15000);
+    })
+
+    b = 1;
+    i = 20;
+    log("hello")
+    log(i)
+    b = 20;
+    log(b*i == i*b)
+    
+    fun();
+
+    std::cout << \
+        "[main] return line." <<\
+        std::endl;
+
+    return 0;
+}
